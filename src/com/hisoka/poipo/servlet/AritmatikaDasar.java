c3dfb70abc5318916fc430c2c4d8b5d13b5e535c
Maryadi <maryadi@elephanttalk.com>
2016-05-16 16:42:20 +0700

    Nambain webservice....

---
+++ src/com/hisoka/poipo/servlet/AritmatikaDasar.java
@@ -0,0 +1,15 @@
+package com.hisoka.poipo.servlet;
+
+public class AritmatikaDasar {
+
+	public int addTen(int nilai)
+	{
+		return (nilai+10);
+	}
+	
+	public int subtractTen(int nilai)
+	{
+		return nilai - 10;
+	}
+	
+}
